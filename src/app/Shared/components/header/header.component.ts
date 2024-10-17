97f3ec2cc14b9541b83b6d9aa8c7c4b20609539a
Daniel Rod <jkdaniel06@gmail.com>
2024-10-17 11:46:47 -0600

    Se implementó el diseño de Header

--- src/app/Shared/components/header/header.component.ts
+++ src/app/Shared/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
