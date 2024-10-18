df83ee81ef205c458b5429442ea3bd2528cd1d14
Daniel Rod <jkdaniel06@gmail.com>
2024-10-17 18:46:06 -0600

    Se realizó el diseño de la pantalla Producto

--- src/app/inventoryManagement/pages/products/products.component.ts
+++ src/app/inventoryManagement/pages/products/products.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FormComponent } from './components/form/form.component';
+import { ActionButtonsComponent } from './components/action-buttons/action-buttons.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [FormComponent, ActionButtonsComponent],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-export default class ProductsComponent {
-
-}
+export default class ProductsComponent {}
